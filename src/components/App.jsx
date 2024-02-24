e4fe8b9eeaa1cdb0830428d1a1f06c31a9c592c9
Massa Timothée <timothee.massa.perso@gmail.com>
2024-02-24 23:19:48 +0100

    Basic Router setup installed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import '../styles.css'
+import Home from './Home'
+import About from './About'
 
 export default function App() {
   return (
-    <main>
-
-    </main>
+      <Router>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>  
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+        </Routes>
+      </Router>
   )
 }
