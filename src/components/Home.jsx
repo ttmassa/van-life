6583ccdd8baf9031a48cefa5c2ebd7773c15b201
Massa Timothée <timothee.massa.perso@gmail.com>
2024-02-25 22:35:16 +0100

    Home main section stylised

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,21 @@ import Header from './Header'
 
 export default function Home() {
     return (
-        <main>
+        <main className='home--container'>
             <Header />
+            <section className='home--main'>
+                <h2 className='home--title'>
+                    You got the travel plans, 
+                    we got the travel vans.
+                </h2>
+                <p className='home--descr'>
+                    Add adventure to your life 
+                    by joining the #vanlife movement.
+                    Rent the perfect van to make 
+                    your perfect road trip.
+                </p>
+                <button className='home--button'>Find your van</button>
+            </section>
         </main>
     )
 }
