54391251a88365780f79175d10feef0d77d746a0
Massa Timothée <timothee.massa.perso@gmail.com>
2024-02-28 16:46:36 +0100

    Route for the Vans page added

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import '../styles.css'
 import Home from './Home'
 import About from './About'
+import Vans from './Vans'
 import Header from '../components/Header'
 
 export default function App() {
@@ -13,7 +14,7 @@ export default function App() {
           <Routes>
             <Route path='/' element={<Home />}/>
             <Route path='/about' element={<About activeLink="/about"/>}/>
-            <Route path='/vans'/>
+            <Route path='/vans' element={<Vans />}/>
           </Routes>
         </div>
       </Router>
