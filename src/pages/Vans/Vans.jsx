ef0bc88b28820eb58496e00ef5e92a3b408a862c
Massa Timothée <timothee.massa.perso@gmail.com>
2024-03-02 13:56:52 +0100

    add a van folder inside pages to reorganize project structure

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import Van from '../components/Van'
+import Van from '../../components/Van'
 
 export default function Vans() {
     const [vans, setVans] = useState([])
