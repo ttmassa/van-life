ba07c300ef2ea4b50095894d73b82c2a7ba65033
Massa Timothée <timothee.massa.perso@gmail.com>
2024-03-04 19:41:13 +0100

    fetch all hosted vans when HostVans component renders

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function HostVans() {
+    const [vans, setVans] = useState([])
+
+    useEffect(() => {
+        fetch('/api/host/vans')
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+        console.log(vans)
+    }, [])
+
     return (
        <div>
             <h1>Host Vans List</h1>
