d9954816ba3e3e93e057eb72e8bc502d9c794f22
Massa Timothée <timothee.massa.perso@gmail.com>
2024-03-06 19:32:40 +0100

    resolved missing dependecies in useEffect

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -3,29 +3,45 @@ import { useState, useEffect } from 'react'
 import HostedVan from '../../components/HostedVan'
 
 export default function HostVans() {
-    const [vans, setVans] = useState([])
+  const [vans, setVans] = useState([])
 
-    useEffect(() => {
-        fetch('/api/host/vans')
-            .then(res => res.json())
-            .then(data => setVans(data.vans))
-        console.log(vans)
-    }, [])
+  useEffect(() => {
+    let isMounted = true
 
-    const hostVansList = vans.map(van => {
-        return <HostedVan 
-            key={van.id}
-            id={van.id}
-            image={van.imageUrl}
-            name={van.name}
-            price={van.price}
-        />
-    })
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/host/vans')
+        const data = await response.json()
 
-    return (
-       <div className='hostvan--page'>
-            <h2>Your listed vans</h2>
-            {hostVansList}
-       </div> 
-    )
+        if (isMounted) {
+          setVans(data.vans)
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchData()
+
+    return () => {
+      isMounted = false
+    }
+  }, []) 
+  
+  const hostVansList = vans.map((van) => (
+    <HostedVan
+      key={van.id}
+      id={van.id}
+      image={van.imageUrl}
+      name={van.name}
+      price={van.price}
+    />
+  ))
+
+  return (
+    <div className='hostvan--page'>
+      <h2>Your listed vans</h2>
+      {hostVansList}
+    </div>
+  )
 }
