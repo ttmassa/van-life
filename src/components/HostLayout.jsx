4c6be452331500d99ff6f25f4ca15610260d67b9
Massa Timothée <timothee.massa.perso@gmail.com>
2024-03-06 18:01:21 +0100

    change dashboard path to a relative path using "."

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -5,7 +5,7 @@ export default function HostLayout() {
     return (
         <div className='hostlayout--main'>
             <nav className='hostlayout--header'>
-                <NavLink to="/host" end className={({ isActive }) => isActive ? "host--active-link" : "hostlayout--link"} >Dashboard</NavLink>
+                <NavLink to="." end className={({ isActive }) => isActive ? "host--active-link" : "hostlayout--link"} >Dashboard</NavLink>
                 <NavLink to="reviews" className={({ isActive }) => isActive ? "host--active-link" : "hostlayout--link"} >Reviews</NavLink>
                 <NavLink to="vans" className={({ isActive }) => isActive ? "host--active-link" : "hostlayout--link"} >Vans</NavLink>
                 <NavLink to="income" className={({ isActive }) => isActive ? "host--active-link" : "hostlayout--link"} >Income</NavLink>
